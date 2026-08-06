c9e7ef8e9fa1aeb19e9f5416f33e0bc078f5253d
Mazhar <saleemmazhar348@gmail.com>
2026-08-06 15:38:30 +0500

    continue_watching_provider method refactored

--- lib/features/courses/data/provider/continue_watching_provider.dart
+++ lib/features/courses/data/provider/continue_watching_provider.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lms/features/courses/data/model/course_model.dart';
+import 'package:lms/features/courses/data/model/course_enrollment_model.dart';
 import 'package:lms/features/courses/data/services/course_service.dart';
 
-final continueWatchingProvider = StreamProvider<List<CourseModel>>((ref) {
-  return CourseService().getCoursesWithEnrollment();
+final continueWatchingProvider = StreamProvider<List<CourseEnrollmentModel>>((ref) {
+  final user = FirebaseAuth.instance.currentUser;
+
+  if (user == null) {
+    return Stream.value([]);
+  }
+  return CourseService().getCoursesWithEnrollment(user.uid);
 });
