0d41ad6b2a4efeeb0f4cd8866d7cbe5af2367d1a
Mazhar <saleemmazhar348@gmail.com>
2026-08-07 12:56:43 +0500

    my_courses provider refactored

--- lib/features/courses/data/provider/course_provider.dart
+++ lib/features/courses/data/provider/course_provider.dart
@@ -5,7 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lms/features/auth/data/providers/auth_provider.dart';
 import 'package:lms/features/courses/data/dummy_data/dummy_course_list.dart';
 import 'package:lms/features/courses/data/model/course_enrollment_model.dart';
-import 'package:lms/features/enrollment/data/model/enrollment_model.dart';
 import 'package:lms/features/enrollment/data/provider/enrollment_provider.dart';
 
 import '../model/course_model.dart';
@@ -27,23 +26,25 @@ final courseDetailProvider = FutureProvider.family<CourseModel, String>((ref, co
   return CourseModel.fromFirestore(doc);
 });
 
-// Course + Whether the user is enrolled in it
+// Course + Enrollment => User's courses
 final courseEnrollmentsProvider = FutureProvider<List<CourseEnrollmentModel>>((ref) async {
   final courses = await ref.watch(coursesProvider.future);
-  final userId = ref.watch(currentUserProvider)?.id;
 
-  final enrollments = userId == null
-      ? List<EnrollmentModel?>.filled(courses.length, null)
-      : await Future.wait(
-          courses.map(
-            (course) => ref.watch(enrollmentLookupProvider((courseId: course.id, userId: userId)).future),
-          ),
-        );
+  final user = ref.watch(currentUserProvider);
 
-  return List.generate(
-    courses.length,
-    (index) => CourseEnrollmentModel(course: courses[index], enrollment: enrollments[index]),
+  if (user == null) {
+    return courses.map((course) => CourseEnrollmentModel(course: course, enrollment: null)).toList();
+  }
+
+  final enrollments = await Future.wait(
+    courses.map(
+      (course) => ref.watch(enrollmentLookupProvider((courseId: course.id, userId: user.id)).future),
+    ),
   );
+
+  return List.generate(courses.length, (index) {
+    return CourseEnrollmentModel(course: courses[index], enrollment: enrollments[index]);
+  });
 });
 
 // Courses where progress > 0
